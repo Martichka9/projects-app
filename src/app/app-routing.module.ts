0496d7b4a1ceaa4a27eef3254339c598da072dd5
Martichka9 <Martichka9@users.noreply.github.com>
2019-10-01 16:22:08 +0300

    create app
    components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from './project/project.component';
+import { ProjectsListComponent } from './projects-list/projects-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'projects-list', component: ProjectsListComponent},
+  {path: 'prj/:id', component: ProjectComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
+  declarations: [
+    HomeComponent,
+    ProjectComponent,
+    ProjectsListComponent
+  ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
---
+++ src/app/models/project.model.ts
@@ -0,0 +1,10 @@
+export class IProject {
+    id: string;
+    company_name: string;
+    comments: number;
+    publish_date: string;
+    contact_name: string;
+    title: string;
+    location_city: string;
+    description: string;
+}
